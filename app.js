56a2aec8b40090e1574c029aa379ab698429aae0
nadeen-nadim <68544006+nadeen-nadim@users.noreply.github.com>
2021-01-21 18:54:48 +0200

    audio_update

--- app.js
+++ app.js
@@ -263,6 +263,23 @@ function animate() {
   renderer.render(scene, camera);
 }
 
+//audio display
+var listener = new THREE.AudioListener();
+camera.add( listener );
+
+// create a global audio source
+var sound = new THREE.Audio( listener );
+
+var audioLoader = new THREE.AudioLoader();
+
+//Load a sound and set it as the Audio object's buffer
+audioLoader.load( 'billie.mp3', function( buffer ) {
+    sound.setBuffer( buffer );
+    sound.setLoop(true);
+    sound.setVolume(0.5);
+    sound.play();
+});
+
 //init();
 
 
